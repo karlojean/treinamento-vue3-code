4e18d03dbb688cbb97547d20c4947519cb6e1f25
karlojean <jeankarlodev@gmail.com>
2024-03-31 22:53:16 -0300

    init project

---
+++ widget/src/App.vue
@@ -0,0 +1,8 @@
+<template>
+  <h1 class="text-red-700">Widget</h1>
+  <widget />
+</template>
+
+<script setup lang="ts">
+import Widget from "./views/Widget/index.vue";
+</script>
---
+++ widget/src/main.ts
@@ -0,0 +1,16 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+import Playground from "./views/Playground/index.vue";
+import { setup } from "./utils/bootstrap";
+import "@/assets/css/tailwind.css";
+import "@/assets/css/font.css";
+import "animate.css";
+
+setup({
+  onProduction: () => {
+    createApp(App).mount("#app");
+  },
+  onDevelopment: () => {
+    createApp(Playground).mount("#app");
+  },
+});
